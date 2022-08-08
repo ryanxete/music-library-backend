1d451cd9b8945b5b60fa42f32ef877782e518ee0
Ryan Etedali <ryanetedali@gmail.com>
2022-08-08 12:04:56 -0500

    created localsetting and imported to settings

---
+++ music_library_backend/local_setting.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-75fzgol_y)mdtd1*^!ojn1r)awn@1_9jjqbtk$1+b98vtl0#86'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_library_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password'
+    }
+}
+
--- music_library_backend/settings.py
+++ music_library_backend/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-75fzgol_y)mdtd1*^!ojn1r)awn@1_9jjqbtk$1+b98vtl0#86'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -69,18 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'music_library_backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -121,3 +106,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from music_library_backend.local_setting import *
+except ImportError:
+    pass

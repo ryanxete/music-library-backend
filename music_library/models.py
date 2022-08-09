29617b543ce2f73640ab31db5d32f73463535ff2
Ryan Etedali <ryanetedali@gmail.com>
2022-08-09 17:44:46 -0500

    created music model and added to the installed app

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class music(models.Model):
+    artist = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    release_date = models.IntegerField()
+    album = models.CharField(max_length=100)

5dc873c269a2f534477b9f4558dabe97b4c85b77
Ryan Etedali <ryanetedali@gmail.com>
2022-08-09 18:14:56 -0500

    created MusicSerializer and file

--- music_library/models.py
+++ music_library/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class music(models.Model):
+class Music(models.Model):
     artist = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     release_date = models.IntegerField()

7080c61b78a84e9e2cfe030ea03b8915f610d923
Ryan Etedali <ryanetedali@gmail.com>
2022-08-10 03:01:55 -0500

    updated urls + worked on views.py

--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .serializers import MusicSerializer
+from rest_framework import status
 
-# Create your views here.
+
+@api_view(['GET'])
+def music_list(request):
+
+    return Response('yea')

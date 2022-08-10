58b6833cfa7fab50cd551949b9562d66d0c0c8cd
Ryan Etedali <ryanetedali@gmail.com>
2022-08-10 03:38:18 -0500

    installed django-cors-headers

--- music_library_backend/settings.py
+++ music_library_backend/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music_library'
+    'music_library',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -46,7 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'music_library_backend.urls'
 

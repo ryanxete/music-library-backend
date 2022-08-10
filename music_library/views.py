ecc37d470d5dfb5c1c09785fcf77b44f93bab067
Ryan Etedali <ryanetedali@gmail.com>
2022-08-10 03:42:09 -0500

    added CRUD functionality

--- music_library/views.py
+++ music_library/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .models import Music
 from .serializers import MusicSerializer
 from rest_framework import status
-
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET','POST'])
 def music_list(request):
@@ -16,3 +16,19 @@ def music_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+def product_detail(request, pk):
+    song = get_object_or_404(Music,pk=pk)
+    if request.method == 'GET':
+        serializer = MusicSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MusicSerializer(song,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response('the song is deleted', status=status.HTTP_204_NO_CONTENT)
+    
